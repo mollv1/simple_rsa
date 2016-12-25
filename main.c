1ff7a48364a878a6d5637599aeb7d39dee5e38c8
Valerio Mollet <valerio.mollet@gmail.com>
2016-12-25 14:14:49 +0100

    add nextPublicKey and privateKey functions

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ typedef struct KeyPair{
 void generateKeys(unsigned prime1, unsigned prime2, KeyPair *keyPairs_p);
 
 unsigned cryptCharacter(unsigned key, unsigned primeProduct, unsigned character);
+unsigned nextPublicKey(unsigned publicKey, unsigned coPrimeProduct);
+unsigned privateKey(unsigned publicKey, unsigned coPrimeProduct);
 
 void clearBuffer();
 
@@ -23,8 +25,8 @@ int main(void)
     printf("choice:\tpublic Key:\tprivate Key:\n");
     for (unsigned index = 0; index < MAX_EXP_KEYS; index++){
         printf("%d.\t%d\t\t%d\n", index +1,
-                                  keyPairs[index].publicKey,
-                                  keyPairs[index].privateKey);
+               keyPairs[index].publicKey,
+               keyPairs[index].privateKey);
     }
 
     puts("Which key pair do you want?");
@@ -46,14 +48,14 @@ int main(void)
         if(message[index] == '\n')
             message[index] = '\0';
         encryptMessage[index] = cryptCharacter(keyPairs[choice-1].publicKey,
-                                               primeProduct,message[index]);
+                primeProduct,message[index]);
         message[index] = encryptMessage[index];
     }
     printf("%s\n",message);
 
     for(unsigned index = 0; index < MAX_MESSAGE && message[index] != '\0';index++){
         decryptMessage[index] = cryptCharacter(keyPairs[choice-1].privateKey,
-                                               primeProduct, encryptMessage[index]);
+                primeProduct, encryptMessage[index]);
         message[index] = decryptMessage[index];
     }
     printf("%s\n",message);
@@ -67,60 +69,73 @@ int main(void)
  * @param prime2 Second Prime used to generate keypairs
  * @param keyPairArray_p Pointer to array where keypairs are written into
  */
-void generateKeys(unsigned prime1, unsigned prime2, KeyPair *keyPairs_p) {
+void generateKeys(unsigned prime1, unsigned prime2, KeyPair *keyPairs_p)
+{
     unsigned publicKey = 1;
     unsigned coPrimeProduct = (prime1 - 1) * (prime2 -1);
-    unsigned testPublicKey = 0;
     //find key pairs till Array is full
     for (unsigned index = 0; index < MAX_EXP_KEYS; index++) {
-
-        //calculate till a puplic key doesnt share a factor with coPrimeProduct gcd == 1
-        do {
-            //only check publicKey >= 3 and uneven values, because coPrimeProduct is allways even
-            publicKey += 2;
-            testPublicKey = publicKey;
-            unsigned check = coPrimeProduct;
-
-            //find gcd from possible public key and coPrime
-            while (check != 0) {
-                unsigned interimResult = testPublicKey % check;
-                testPublicKey = check;
-                check = interimResult;
-            }
-        }
-        while(testPublicKey != 1);
-
-        //store the public key
+        publicKey = nextPublicKey(publicKey, coPrimeProduct);
         keyPairs_p->publicKey = publicKey;
-
-        //create an array for the execution of the extended euclidean algorithm
-        int calcPrivateKey[2][2]= {{coPrimeProduct, coPrimeProduct},{publicKey, 1}};
-
-        //find private Key with the extended euclidean algorithm
-        while (calcPrivateKey[1][0] != 1) {
-            unsigned tempResult1 = calcPrivateKey[0][0] % calcPrivateKey[1][0];
-            int tempResult2 = calcPrivateKey[0][1] - calcPrivateKey[1][1]*(calcPrivateKey[0][0]/calcPrivateKey[1][0]);
-            while (tempResult2 < 0)
-                tempResult2 += coPrimeProduct;
-            calcPrivateKey[0][0] = calcPrivateKey[1][0];
-            calcPrivateKey[0][1] = calcPrivateKey[1][1];
-            calcPrivateKey[1][0] = tempResult1;
-            calcPrivateKey[1][1] = tempResult2;
-        }
-
-        //store private key
-        keyPairs_p->privateKey = calcPrivateKey[1][1];
+        keyPairs_p->privateKey = privateKey(publicKey, coPrimeProduct);
         keyPairs_p++;
     }
 }
+/**
+ * @brief find next publicKey, which share no factor with coPrimeProduct
+ * @param publicKey
+ * @param coPrimeProduct
+ */
+unsigned nextPublicKey(unsigned publicKey, unsigned coPrimeProduct)
+{
+    unsigned nextKey = publicKey;
+    int gcd=0;
+    do {
+        //only check publicKey >= 3 and uneven values, because coPrimeProduct is allways even
+        nextKey += 2;
+        unsigned check = coPrimeProduct;
 
+        //find gcd from possible public key and coPrime
+        gcd = nextKey;
+        while (check != 0) {
+            unsigned interimResult = gcd % check;
+            gcd = check;
+            check = interimResult;
+        }
+    } while(gcd != 1);
+    return  nextKey;
+}
+
+/**
+ * @brief calculate privateKey from publicKey with euclidean algorithm
+ * @param publicKey
+ * @param coPrimeProduct
+ */
+unsigned privateKey(unsigned publicKey, unsigned coPrimeProduct)
+{
+    int calcPrivateKey[2][2]= {{coPrimeProduct, coPrimeProduct},{publicKey, 1}};
+
+    //find private Key with the extended euclidean algorithm
+    while (calcPrivateKey[1][0] != 1) {
+        unsigned tempResult1 = calcPrivateKey[0][0] % calcPrivateKey[1][0];
+        int tempResult2 = calcPrivateKey[0][1] - calcPrivateKey[1][1]*(calcPrivateKey[0][0]/calcPrivateKey[1][0]);
+        while (tempResult2 < 0)
+            tempResult2 += coPrimeProduct;
+        calcPrivateKey[0][0] = calcPrivateKey[1][0];
+        calcPrivateKey[0][1] = calcPrivateKey[1][1];
+        calcPrivateKey[1][0] = tempResult1;
+        calcPrivateKey[1][1] = tempResult2;
+    }
+    return calcPrivateKey[1][1];
+}
 /**
  * @brief encrypts or decrypts a character with a key
  * @param key Public key that is used to encrypt or decrypt the character
  * @param primeProduct
  * @param character
  */
-unsigned cryptCharacter(unsigned key, unsigned primeProduct, unsigned character) {
+unsigned cryptCharacter(unsigned key, unsigned primeProduct, unsigned character)
+{
     unsigned long cryptResult = 1;
 
     //calculate character ^ key mod primeproduct = cryptCharacter
@@ -131,7 +146,8 @@ unsigned cryptCharacter(unsigned key, unsigned primeProduct, unsigned character)
     return cryptResult;
 }
 
-void clearBuffer(){
+void clearBuffer()
+{
     char c;
     while((c = getchar()) != '\n' && c != EOF);
 }
